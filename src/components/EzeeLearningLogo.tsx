1b807c3a40df62ef520ab6bad8e9f614daf220cf
Heinrich-Swart <swartheinrich12@gmail.com>
2025-05-15 00:22:03 +0200

    Initial commit

---
+++ src/components/EzeeLearningLogo.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function EzeeLearningLogo() {
+  return (
+    <img
+      src="/logo-transparent.svg"
+      alt="EzeeLearning Logo"
+      style={{
+        height: '100px',
+        width: 'auto',
+        display: 'block',
+      }}
+    />
+  );
+}
