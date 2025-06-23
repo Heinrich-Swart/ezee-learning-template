1cbba7fa664f98637b3964b9b82ad742d59c47f4
Heinrich-Swart <swartheinrich12@gmail.com>
2025-06-23 22:35:32 +0200

    Next issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
