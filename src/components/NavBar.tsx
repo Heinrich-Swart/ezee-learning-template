fe7ecc7b958209f90f773efcd9563c3e4e3424c1
Heinrich-Swart <swartheinrich12@gmail.com>
2025-06-30 13:56:47 +0200

    Fixed build error

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,7 +14,6 @@ import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import EzeeLearningLogo from './EzeeLearningLogo';
-import ColorModeIconDropdown from '@/shared-theme/ColorModeIconDropdown';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
